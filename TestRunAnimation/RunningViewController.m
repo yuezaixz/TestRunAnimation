265b45c6a3ead6e72023e8f9e971c5940859e7d2
yuezaixz <303178394@qq.com>
2015-12-08 21:52:09 +0800

    尝试画贝塞尔曲线运动轨迹

--- TestRunAnimation/RunningViewController.m
+++ TestRunAnimation/RunningViewController.m
@@ -12,7 +12,9 @@
 
 @end
 
-@implementation RunningViewController
+@implementation RunningViewController{
+    NSInteger currentIndex;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -25,10 +27,89 @@
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
 }
 - (IBAction)actionStop:(id)sender {
-    [self dismissViewControllerAnimated:YES completion:nil];
+    if (currentIndex == 0 ) {
+        UIBezierPath *aPath = [UIBezierPath bezierPath];
+        
+        aPath.lineWidth = 5.0;
+        aPath.lineCapStyle = kCGLineCapRound;  //线条拐角
+        aPath.lineJoinStyle = kCGLineCapRound;  //终点处理
+        
+        [aPath moveToPoint:CGPointMake(246, 91)];
+        
+        [aPath addQuadCurveToPoint:CGPointMake(120, 177) controlPoint:CGPointMake(192, 147)];
+        
+        [aPath stroke];
+        
+        //（2）加到CAShapeLayer，效果出来了
+        
+        CAShapeLayer *shapelayer = [CAShapeLayer layer];
+        
+        shapelayer.path = aPath.CGPath;
+        //设置边框颜色
+        
+        shapelayer.strokeColor = [[UIColor redColor] CGColor];
+        
+        //设置填充颜色
+        
+        shapelayer.fillColor = [[UIColor clearColor] CGColor];
+        
+        
+        [self.animationImageview.layer addSublayer:shapelayer];
+        
+        
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+        animation.fromValue = @(0.0);
+        animation.toValue = @(1.0);
+        shapelayer.autoreverses = NO;
+        animation.duration = 1.0;
+        [shapelayer addAnimation:animation forKey:nil];
+        shapelayer.strokeEnd = 1;
+        currentIndex++;
+    } else if (currentIndex == 1 ) {
+        UIBezierPath *aPath = [UIBezierPath bezierPath];
+        
+        aPath.lineWidth = 5.0;
+        aPath.lineCapStyle = kCGLineCapRound;  //线条拐角
+        aPath.lineJoinStyle = kCGLineCapRound;  //终点处理
+        
+        [aPath moveToPoint:CGPointMake(120, 177)];
+        
+        [aPath addQuadCurveToPoint:CGPointMake(103, 144) controlPoint:CGPointMake(102, 79)];
+        
+        [aPath stroke];
+        
+        //（2）加到CAShapeLayer，效果出来了
+        
+        CAShapeLayer *shapelayer = [CAShapeLayer layer];
+        
+        shapelayer.path = aPath.CGPath;
+        //设置边框颜色
+        
+        shapelayer.strokeColor = [[UIColor redColor] CGColor];
+        
+        //设置填充颜色
+        
+        shapelayer.fillColor = [[UIColor clearColor] CGColor];
+        
+        
+        [self.animationImageview.layer addSublayer:shapelayer];
+        
+        
+        CABasicAnimation *animation = [CABasicAnimation animationWithKeyPath:@"strokeEnd"];
+        animation.fromValue = @(0.0);
+        animation.toValue = @(1.0);
+        shapelayer.autoreverses = NO;
+        animation.duration = 1.0;
+        [shapelayer addAnimation:animation forKey:nil];
+        currentIndex ++;
+    } else if (currentIndex == 2 ) {
+        [self dismissViewControllerAnimated:YES completion:nil];
+        currentIndex = 0;
+    }
+    
+    
 }
 
 @end
